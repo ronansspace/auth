162e898d296804490a18baebdba7ae6cfc6eda3e
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-03-01 23:58:07 +0000

    pair ID editable

--- admin/index.php
+++ admin/index.php
@@ -214,7 +214,6 @@ if (tdrLoggedIn()) {
                         <th>Contract</th>
                         <th>OrderID</th>
                         <th>Pair</th>
-                        <th>Base</th>
                         <th>GrossProfit</th>
                         <th>BrokerCost</th>
                         <th>VenueCost</th>
@@ -679,7 +678,7 @@ include('footer.php');
                         for (var i = 0; i < s.length; i++) {
 
                             pTable.fnAddData([
-                                s[i][0], s[i][1], s[i][2], s[i][3], s[i][4], s[i][5], s[i][6], s[i][7], s[i][8]
+                                s[i][0], s[i][1], s[i][2], s[i][4], s[i][5], s[i][6], s[i][7], s[i][8]
                             ]);
 
                         } // End For
