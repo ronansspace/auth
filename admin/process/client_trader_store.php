aca3a66dcf55598131d4e29b271366d47a30976e
ronieh <ronanhughes@Ronans-MacBook-Pro.local>
2017-03-15 21:59:28 +0000

    trader store

--- admin/process/client_trader_store.php
+++ admin/process/client_trader_store.php
@@ -18,17 +18,27 @@ if(tdrLoggedIn()){
     $email_bcc = $_POST['email_cc'];
     $trader_id = $_POST['trader_id'];
     $broker = $_POST['broker'];
+    $clientName = "";
+
+    $sql2 = "select * from clients where id = '$client'";
+    $result2 = $conn->query($sql2);
+    $fetch2 = $result2->fetch_array();
+
+    if($fetch2['name'] <> ""){
+        $client_name = $fetch2['name'];
+    }
+
 
     $sql = "";
     
     if($id_client == ""){
     
-          $sql = "insert into client_trader(name, email, email_cc, client_id, broker, email_bcc)
-                  values ('$client', '$email', '$email_cc', $trader_id, '$broker', '$email_bcc')";
+          $sql = "insert into client_trader(name, email, email_cc, client_id, broker, email_bcc, ClientName)
+                  values ('$client', '$email', '$email_cc', $trader_id, '$broker', '$email_bcc', '$client_name')";
                       
     } else {
     
-          $sql = "update client_trader set broker = '$broker', name = '$client', email = '$email', email_cc = '$email_cc', client_id = $trader_id
+          $sql = "update client_trader set broker = '$broker', name = '$client', email = '$email', email_cc = '$email_cc', email_bcc = '$email_bcc', client_id = $trader_id, ClientName = '$client_name'
                   where id = $id_client";    
     }
     
